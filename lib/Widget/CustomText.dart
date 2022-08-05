73c50578296bf599749ab18b0b01e26b7b367d89
Hamza Ahmad <hamzaa.malik194@gmail.com>
2022-08-05 06:32:08 +0500

    Fixes and Updates

--- lib/Widget/CustomText.dart
+++ lib/Widget/CustomText.dart
@@ -4,12 +4,12 @@ class CustomText extends StatelessWidget {
   final String text;
   final double textsize;
   final Color color;
-  final double? letterSpacing;
-  final FontWeight? fontWeight;
+  final double letterSpacing;
+  final FontWeight fontWeight;
   CustomText(
-      {required this.text,
-      required this.textsize,
-      required this.color,
+      {this.text,
+      this.textsize,
+      this.color,
       this.letterSpacing,
       this.fontWeight});
   @override
